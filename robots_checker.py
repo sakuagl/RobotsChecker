799fb18035276d1d4a6097e183bba31fb958edad
sakuagl <shuoyeshantian@gmail.com>
2023-06-06 14:13:57 +0900

    httpステータスが200番台以外の場合、エラーを発生させるメソッドを作成

--- robots_checker.py
+++ robots_checker.py
@@ -1,26 +1,12 @@
 import requests
 from urllib.parse import urlparse, urljoin
-from requests.exceptions import RequestException
 
 class RobotsChecker:
-    """
-    This class checks whether a given URL is allowed to be accessed by robots.txt file.
-    """
     
     def __init__(self, user_agent='*') -> None:
         self.user_agent = user_agent
 
     def is_allowed(self, url: str) -> bool:
-        """
-        Determines whether the specified URL is allowed to be accessed by robots.txt file.
-        Unless you specify otherwise in your robots.txt file, all files are implicitly allowed for crawling.
-
-        Args:
-            url (str): URL to check access permission.
-
-        Returns:
-            bool: True if access is allowed, False if not.
-        """
         # Parse the base URL from the given URL.
         parsed_url = urlparse(url)
         base_url = f"{parsed_url.scheme}://{parsed_url.netloc}"
@@ -31,13 +17,45 @@ class RobotsChecker:
         # Attempt to retrieve the content of the robots.txt file.
         try:
             response = requests.get(robots_url)
+            # response.status_code = 100
+            self._raise_status(response)
             content = response.text
-        except RequestException as e:
+        except StatusError as e:
+            print("An Exception occurred:", str(e))
+        except requests.RequestException as e:
             # If an error occurs, assume that access is not allowed.
-            print("A exception occurred:\n", str(e))
+            # print("A error occurred:", str(e))
             return False
 
         return True
+        
+    def _raise_status(self, response: requests.Response) -> None:
+        """
+        Raises an exception if the HTTP status code is not in the 200 range.
+
+        Args:
+            response (requests.Response): The HTTP response object.
+
+        Raises:
+            StatusError: If the HTTP status code is not in the 200 range.
+        """
+        status_code = response.status_code
+        if status_code // 100 != 2:
+            raise StatusError("Unexpected HTTP status code", status_code)
+        
+    def check_access(self) -> bool:
+        pass
+        
+    def _path_matches(self, url: str) -> bool:
+        pass
+
+class StatusError(Exception):
+    """
+    This is a custom exception class that inherits from the built-in Exception class.
+    It is used to raise exceptions in case of certain errors in the program.
+    """
+    def __init__(self, message: str, status_code: int) -> None:
+        super().__init__(f"{message}: {status_code}")
 
 if __name__ == '__main__':
     checker = RobotsChecker()
